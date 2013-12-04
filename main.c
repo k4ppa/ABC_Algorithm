0bfad32df43ed6a804965a0f0f84b2c57023584a
Mattia Cappa <cappa.mattia@gmail.com>
2013-12-04 16:28:29 +0100

    Main pulito dal test.

--- main.c
+++ main.c
@@ -2,21 +2,9 @@
 #include "types.h"
 #include "sphere.h"
 
-void funzione(int prova[])
-{
-	prova[1] = 9;
-}
 int main()
 {
-	Flag var[3];
 	Bees bees;
-	//int prova[3] = {0, 1, 2};
-	//funzione(prova);
-	//printf("%d%d%d\n", prova[0], prova[1], prova[2]);
 
-	setBits(&var[1], 1);
-	printf("bits %d\n", getBits(&var[1]));
-
-	system("PAUSE");
 	return 0;
 }
