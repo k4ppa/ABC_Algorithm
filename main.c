4565582f00501b5c3660b7894deb59d11bb84b66
Mattia Cappa <cappa.mattia@gmail.com>
2013-12-04 22:56:54 +0100

    Print per stampare lo stato di bees fatte.
    BUG: piu' della meta' delle api sono employed.

--- main.c
+++ main.c
@@ -1,13 +1,42 @@
 #include "bees.h"
 #include "types.h"
 #include "sphere.h"
+	
+void printPosition(float position[])
+{
+	int y;
+	printf("position: ");
+	for(y=0; y<D; y++)
+		printf("%f\t", position[y]);
+	printf("\n");
+}
+
+void printBees(Bees bees)
+{
+	int i;
+	for (i=0; i<SN; i++)
+	{
+		printf("type: %d\n", bees->type[i].bits);
+		printf("fitness: %f\n", bees->fitness[i]);
+		printPosition(bees->positions[i]);
+		printf("trial: %d\n", bees->trial[i]);
+		printf("p: %f\n\n", bees->p[i]);
+	}
+}
+
 
 int main()
 {
 	Bees bees = NULL;
+	int i;
 	bees = (Bees) malloc(sizeof (struct bees));
 
 	initializeBees(bees);
 
+	for(i=0; i<SN; i++)
+		employedPlacement(bees, i);
+	printBees(bees);
+
+	system("PAUSE");
 	return 0;
 }
