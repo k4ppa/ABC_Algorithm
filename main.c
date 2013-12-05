abd0ed4e3346ba50e071dd36a07abfa053480066
Mattia Cappa <cappa.mattia@gmail.com>
2013-12-05 18:46:32 +0100

    Rebase effettuato e main pulito dai test.

--- main.c
+++ main.c
@@ -3,50 +3,9 @@
 #include "sphere.h"
 #include <time.h>
 	
-void printPosition(float position[])
-{
-	int y;
-	printf("position: ");
-	for(y=0; y<D; y++)
-		printf("%f\t", position[y]);
-	printf("\n");
-}
-
-void printBees(Bees bees)
-{
-	int i;
-	for (i=0; i<SN; i++)
-	{
-		printf("type: %d\n", bees->type[i].bits);
-		printf("fitness: %f\n", bees->fitness[i]);
-		printPosition(bees->positions[i]);
-		printf("trial: %d\n", bees->trial[i]);
-		printf("p: %f\n\n\n", bees->p[i]);
-	}
-}
-
-
 int main()
 {
 	Bees bees = NULL;
-	int i, k;
-	float values[10];
 	
-	bees = (Bees) malloc(sizeof (struct bees));
-
-	srand(time(0));
-	initializeBees(bees);
-
-	for(i=0; i<SN; i++)
-		employedPlacement(bees, i);
-	printBees(bees);
-
-/*	for (k=0; k<10; k++) 
-		values[k] = chooseRandomValueBetweenRange();
-		
-	for (k=0; k<10; k++)
-		printf("Value %d: %f\n", k, values[k]);
-*/
-	system("PAUSE");
 	return 0;
 }
