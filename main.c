0ffa3756bb561026e34f0208edb89d6e3249ea75
Mattia Cappa <cappa.mattia@gmail.com>
2013-12-15 22:12:08 +0100

    Test di schwefel.
    Il risultato non e' preciso, questo e' dovuto alla perdita dei dati
    causati dall'uso dei float.
    
    E' stato modificato un segno alla formula, l'articolo sostiene che sia
    meno, pero' guardando anche si internet risulta che debba essere piu'.
    Oppure e' piu' il segno prima della sommatoria, uno dei due, e' uguale.

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include "bees.h"
 #include "types.h"
 #include <time.h>
-	
+
+void inizializeValues(float values[])
+{
+	int i;
+	for (i=0; i<D; i++)
+		values[i] = 420.96f;
+}
+
+void printValues(float values[])
+{
+	int i;
+	for (i=0; i<D; i++)
+		printf("%f\t", values[i]);
+	printf("\n");
+}
+
 int main()
 {
 	Bees bees = NULL;
-	
+	float values[D];
+	float a;
+	inizializeValues(values);
+	printValues(values);
+	printf("f: %f\n", formulae(values));
+
+	system("PAUSE");
 	return 0;
 }
