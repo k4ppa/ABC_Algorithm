54c3a7c035b6fb3bd6e7ab10258bd5c20b721fed
Mattia Cappa <cappa.mattia@gmail.com>
2013-12-17 17:09:16 +0100

    Tolto il prototipo di una funzione.
    QUella funzione si trova in main.c e non c'entra con bestPosition.

--- bestPosition.h
+++ bestPosition.h
@@ -1,7 +1,6 @@
 #include "bestBee.h"
 #include "bees.h"
 
-void beesSearch(Bees bees);
 void saveBestPosition(BestBee bestBee, Bees bees);
 	BOOL isBeePositionBetter(BestBee bestBee, Bees bees, int i);
 	void replaceBestPosition(BestBee bestBee, Bees bees, int i);
