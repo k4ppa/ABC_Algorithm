9e4d2b8121c08c4626f86e0a45bd15738289e31d
Mattia Cappa <cappa.mattia@gmail.com>
2013-12-17 19:26:29 +0100

    Refactoring
    Il controllo se bestBee e' gia' inizializzato o no e' fatto solo una volta
    all'inizio. Se non lo e' viese assegnato un valore predefinito, ossia i
    dati dell'ape in posizione zero.

--- bestPosition.h
+++ bestPosition.h
@@ -2,5 +2,8 @@
 #include "bees.h"
 
 void saveBestPosition(BestBee bestBee, Bees bees);
-	BOOL isBeePositionBetter(BestBee bestBee, Bees bees, int i);
-	void replaceBestPosition(BestBee bestBee, Bees bees, int i);
+	void controlInizialization(BestBee bestBee, Bees bees);
+		BOOL isNotAlreadyInizialized(BestBee bestBee);
+	void controlPosition(BestBee bestBee, Bees bees, int i);
+		BOOL isBeePositionBetter(BestBee bestBee, Bees bees, int i);
+void replaceBestPosition(BestBee bestBee, Bees bees, int i);
