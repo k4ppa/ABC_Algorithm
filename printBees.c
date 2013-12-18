aef90d489f4db87434e7e75859fc93909482e429
Mattia Cappa <cappa.mattia@gmail.com>
2013-12-18 11:49:41 +0100

    Sistemata formattazione delle stampe

--- printBees.c
+++ printBees.c
@@ -2,28 +2,26 @@
 
 void printBestBee(BestBee bestBee)
 {
-	printf("TYPE\t\t\tFITNESS\t\t\t\tTRIAL\n");
-	printf("best\t\t\t%f\t\t*\n", getBestFitness(bestBee));
+	dprintf("\tB\t*\t%f\n", getBestFitness(bestBee));
 	printPosition(getBestPosition(bestBee));
 }
-
 
 void printBees(Bees bees)
 {
     int i;
-	printf("TYPE\t\t\tFITNESS\t\t\t\tTRIAL\n");
     for (i=0; i<SN; i++)
     {
-        printf("%d\t\t\t%f\t\t%d\n", getType(bees, i), getFitness(bees, i), getTrial(bees, i));
+        dprintf("\t%d\t%d\t%f\n", getType(bees, i), getTrial(bees, i), getFitness(bees, i));
         printPosition(bees->positions[i]);
-		printf("***\t\t\t***\t\t***\n");
+		printf("\n-------------------------------------------------------------------------------\n");
     }
+	printf("--------------------------------------------------------------------------------\n");
 }
 
 void printPosition(float position[])
 {
 	int y;
 	for(y=0; y<D; y++)
-		printf("%f\t", position[y]);
+		dprintf("%f\t", position[y]);
 	printf("\n");
 }
