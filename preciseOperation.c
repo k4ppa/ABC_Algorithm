3ff49322729febc6860664d6117007e36b5da4a6
Mattia Cappa <cappa.mattia@gmail.com>
2014-01-14 15:32:35 +0100

    Aggiunte sottrazione e l'elevazione a potenza

--- preciseOperation.c
+++ preciseOperation.c
@@ -4,7 +4,7 @@ float mul(float number1, float number2)
 {
 	char stringNumber1[25], stringNumber2[25];
 	mpf_t mpf_number1, mpf_number2, result;
-
+	
 	sprintf(stringNumber1, "%.20f", number1);
 	sprintf(stringNumber2, "%.20f", number2);
 
@@ -40,3 +40,45 @@ float add(float number1, float number2)
 	return mpf_get_d(result);
 }
 
+float sub(float number1, float number2)
+{
+	char stringNumber1[25], stringNumber2[25];
+	mpf_t mpf_number1, mpf_number2, result;
+
+	sprintf(stringNumber1, "%.20f", number1);
+	sprintf(stringNumber2, "%.20f", number2);
+
+	mpf_init(mpf_number1);
+	mpf_init(mpf_number2);
+	mpf_init(result);
+
+	mpf_set_str(mpf_number1, stringNumber1, 10);
+	mpf_set_str(mpf_number2, stringNumber2, 10);
+
+	mpf_sub(result, mpf_number1, mpf_number2);
+	
+	return mpf_get_d(result);
+}
+
+float myPow(float base, float exponent)
+{
+	char stringBase[25], stringExponent[10];
+	mpf_t mpf_base, mpf_exponent, result;
+	mpir_ui mpir_exponent;
+
+	sprintf(stringBase, "%.20f", base);
+	sprintf(stringExponent, "%f", exponent);
+
+	mpf_init(mpf_base);
+	mpf_init(mpf_exponent);
+	mpf_init(result);
+
+	mpf_set_str(mpf_base, stringBase, 10);
+	mpf_set_str(mpf_exponent, stringExponent, 10);
+
+	mpir_exponent = mpf_get_ui(mpf_exponent);
+
+	mpf_pow_ui(result, mpf_base, mpir_exponent);
+	
+	return (float) mpf_get_d(result);
+}
--- preciseOperation.h
+++ preciseOperation.h
@@ -1,4 +1,11 @@
+#ifndef PRECISEOPERATION_H_
+#define PRECISEOPERATION_H_
+
 #include "mpir.h"
 
 float mul(float number, float cosNumber);
 float add(float number1, float number2);
+float sub(float number1, float number2);
+float myPow(float base, float exponent);
+
+#endif
